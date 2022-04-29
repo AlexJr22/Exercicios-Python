bf85f978363ef5c12171a1f23b1f1d27c931aec8
Alex Junior <alexjr2oficial@gmail.com>
2022-04-29 11:34:59 -0300

    adicionando comentários aos códigos

--- Exercicios/EX001/EX001.py
+++ Exercicios/EX001/EX001.py
@@ -1,6 +1,6 @@
 '''Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vinda 
 de acordo com o valor digitado'''
 
-nome = str(input('Qual seu nome?: '))           # variavel q recebe o nome do usuario
+nome = str(input('Qual seu nome?: '))           # variavel que recebe o nome do usuario
 
 print(f'Seja bem-vindo \033[33m{nome}\033[m!')  # print que mostra a mensagem de boas-vindas
--- Exercicios/EX002/EX002.py
+++ Exercicios/EX002/EX002.py
@@ -2,9 +2,9 @@
 com a data formatada'''
 from datetime import date
 
-ano = int(input('Qual ano você nasceu?: '))
-mes = str(input('Qual o mês você nasceu?: '))
-idade = date.today().year - ano
+ano = int(input('Qual ano você nasceu?: '))     # variavel que recebe o ano de nascimento
+mes = str(input('Qual o mês você nasceu?: '))   # variavel que recebe o mês de nascimento
+idade = date.today().year - ano                 # variavel que calcula a idade do usuario
 
 print(f'Você nasceu em {ano} no mês de {mes}')
 print(f'Você tem {idade} anos de idade')
