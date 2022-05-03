e74c9b57e1fe4e00665ee3ce19bb79e90aedcdd0
Alex Junior <alexjr2oficial@gmail.com>
2022-05-03 09:36:17 -0300

    atualizando o EX006

--- Exercicios/EX006/Ex006.py
+++ Exercicios/EX006/Ex006.py
@@ -10,4 +10,4 @@ print(f'O dobro de {num} é {num*2}!')
 print(f'O triplo de {num} é {num*3}!')
 
 # calculando a raiz quadrada usando a biblioteca math
-print(sqrt(num))
+print(f'A raiz quadrada de {num} é {sqrt(num)}')
