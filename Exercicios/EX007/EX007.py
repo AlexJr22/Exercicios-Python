031598f76b495917c4f9b849fca5d0d9ebd60c9f
Alex Junior <alexjr2oficial@gmail.com>
2022-05-03 09:45:13 -0300

    criando o EX007

---
+++ Exercicios/EX007/EX007.py
@@ -0,0 +1,8 @@
+'''faça um programa que leia duas notas de um aluno e mostre sua média'''
+
+nota1 = float(input('Primeira nota: ')) # recebe a primeira nota
+nota2 = float(input('Segunda nota: '))  # recebe a segunda nota
+
+# mostrando os resultados
+print(f'As notas do aluna são: {nota1}, {nota2}')
+print(f'A média do aluno é {(nota1+nota2) / 2}')
