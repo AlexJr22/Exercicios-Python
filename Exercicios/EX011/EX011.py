90a9bf6a7274571af8ad17de4546eca1c5aefee6
Alex Junior <alexjr2oficial@gmail.com>
2022-05-05 11:43:45 -0300

    criando o EX011

---
+++ Exercicios/EX011/EX011.py
@@ -0,0 +1,3 @@
+'''DESAFIO 11:
+Faça um programa que leia a altura e a largura de uma parede em metros, calcule sua área e mostre a quantidade de tinta necessaria pra pintá-la, sabendo que cada litro de tinta pinta uma área de 2m².'''
+
