b074ca6bb8ffd30d2026616b8ef8171e47793e35
Alex Junior <alexjr2oficial@gmail.com>
2022-05-09 11:27:29 -0300

    atualizando o EX013

--- Exercicios/EX013/EX013.py
+++ Exercicios/EX013/EX013.py
@@ -1,2 +1,7 @@
 '''DESAFIO 13:
 Faça um programa que leia o sálario de um funcionario e mostre seu novo sálario com, com 15% de aumento.'''
+salario_inic = float(input('Qual o sálario atual do do funcionario?: R$'))# recebe o sálario inicial
+nov_salario = salario_inic + (salario_inic * 15/100)                    # adiciona o aumento 
+
+# resultado
+print(f'O novo sálario do é R${nov_salario:.2f}')
