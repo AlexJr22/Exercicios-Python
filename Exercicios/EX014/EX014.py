7216651a525771ce22b687260dd0d53a4ed574ef
Alex Junior <alexjr2oficial@gmail.com>
2022-05-09 11:32:16 -0300

    atualizando o EX014

--- Exercicios/EX014/EX014.py
+++ Exercicios/EX014/EX014.py
@@ -1,2 +1,8 @@
 '''DESAFIO 14:
 Escreva um programa que converta graus celcius para graus fahrenheit'''
+
+celcius = float(input('Qual a temperatura do ambiente em °C:')) # recebe um valor em Celcius
+fahre = ((celcius * 9) / 5) + 32                                      # converte para fahrenheit
+
+# resultado
+print(f'A temperatura em °F é: {fahre:.2f}')
