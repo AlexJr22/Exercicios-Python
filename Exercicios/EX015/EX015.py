d7e3401f54024c0a464326842bce9672064ca831
Alex Junior <alexjr2oficial@gmail.com>
2022-05-10 15:08:36 -0300

    atualizando o EX015

---
+++ Exercicios/EX015/EX015.py
@@ -0,0 +1,15 @@
+'''DESAFIO 15:
+Escreva um programa que pergunte a quantidade de KM percorridos por um carro alugado e a quantidade de dias que ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60.00 por dia e R$0.15 por KM rodado'''
+
+alug_dia = int(input('Quantos dias o carro foi alugado?: ')) # recebo a quantidade de dias alugados
+KM_rodado = int(input('Quantos KM foram rodados durante o período de aluguel?: ')) # recebe quantos km foram percorridos
+
+valor_alug = 60 * alug_dia
+valor_km = 0.15 * KM_rodado
+
+# resultado
+print('-='*17)
+print('\033[33m|',f'DIAS ALUGADOS: {valor_alug}'.center(30),'|')
+print('|',f'KM RODADOS: {valor_km:.2f}'.center(30),'|')
+print('|',f'VALOR TOTAL: {valor_alug+valor_km}'.center(30),'|\033[m')
+print('=-'*17)
