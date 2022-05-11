33e306bb6e68b1396d3390c222140314c3eca790
Alex Junior <alexjr2oficial@gmail.com>
2022-05-11 11:43:16 -0300

    criando o EX016

---
+++ Exercicios/EX016/EX016.py
@@ -0,0 +1,5 @@
+'''DESAFIO 16:
+Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela sua porção inteira
+EX: digite um nÚumero: 6.127
+    O número 6.127 tem a parte inteira 6
+'''
