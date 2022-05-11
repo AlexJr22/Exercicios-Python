0ea586af81f2200275c5e42cb719c946c2d6e599
Alex Junior <alexjr2oficial@gmail.com>
2022-05-11 11:47:47 -0300

    atualizando o EX016

--- Exercicios/EX016/EX016.py
+++ Exercicios/EX016/EX016.py
@@ -3,3 +3,9 @@ Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela
 EX: digite um nÚumero: 6.127
     O número 6.127 tem a parte inteira 6
 '''
+
+num1 = float(input('Digite um número: '))   # recebe um número float()
+num2 = int(num1)                            # converte o número para int()
+
+# resultado
+print(f'A parte inteira de {num1} é {num2}')
