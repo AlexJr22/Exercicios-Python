82f1654f715d6376018d3a96d0583721de4ce2ef
Alex Junior <alexjr2oficial@gmail.com>
2022-05-12 13:11:13 -0300

    atualizando o EX017

--- Exercicios/EX017/EX017.py
+++ Exercicios/EX017/EX017.py
@@ -2,3 +2,11 @@
 Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo
 e calcule sua hipotenusa.'''
 
+from math import hypot
+
+co = float(input('Qual o valor do cateto oposto: '))    # recebe o cateto oposto
+ca = float(input('Qual o valor do cateto adjacente: ')) # recebe o cateto adjacente
+hipot = hypot(co, ca)   # calcula a hipotenusa
+
+# resultado
+print(f'A hipotenusa desse triângulo retângulo é {hipot:.2f}.')
