6747a3f81ec4f1551e72185a0a8ee86f0e0d5f85
Alex Junior <alexjr2oficial@gmail.com>
2022-05-14 11:17:32 -0300

    terminando o EX018

--- Exercicios/EX018/EX018.py
+++ Exercicios/EX018/EX018.py
@@ -1,3 +1,16 @@
 '''DESAFIO 18:
 Faça um programa que leia um ângulo qualquer e mostre na tela o valor de seno°, cosseno°, e tangente desse ângulo'''
 
+from math import radians, cos, sin, tan
+
+angulo = float(input('Digite um ângulo:'))  # recebe o ângulo
+
+seno = sin(radians(angulo)) # calcula senº
+cose = cos(radians(angulo)) # calcula cosº
+tang = tan(radians(angulo)) # calcula tanº
+
+# resultado
+print(f'| ------- O Senº {angulo} é {seno:.2f} ------- |')
+print(f'| ------- O Cosº {angulo} é {cose:.2f} ------- |')
+print(f'| ------- A tanº {angulo} é {tang:.2f} ------- |')
+    
