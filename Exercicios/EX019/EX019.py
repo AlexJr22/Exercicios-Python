d039c389627fcd76184c54fc0f99295562e29979
Alex Junior <alexjr2oficial@gmail.com>
2022-05-14 11:30:46 -0300

    terminando o EX019

--- Exercicios/EX019/EX019.py
+++ Exercicios/EX019/EX019.py
@@ -1,4 +1,12 @@
 '''DESAFIO 19:
-Um professor quer sortea um dos seus 4 alunos para apagar o quadro. Faça um programa que
-ajude ele, lendo o nome dos alunos e escrevendo o nome deles e o nome do escolhido.'''
+Um professor quer sortea um dos seus 4 alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos 
+alunos e escrevendo o nome deles e o nome do escolhido.'''
 
+from random import choice
+
+alunos = []
+for v in range(0, 4):   # pergunta o nome dos alunos
+    alunos.append(str(input(f'Nome do {v+1}º aluno: ')))
+
+# resultado
+print(choice(alunos))   # sorteia um aluno aleatorio
