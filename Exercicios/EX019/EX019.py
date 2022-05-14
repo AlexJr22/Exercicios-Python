993017a9038418eb0a457cf7c10232f1cd7bcf32
Alex Junior <alexjr2oficial@gmail.com>
2022-05-14 11:23:05 -0300

    Criando o EX019

---
+++ Exercicios/EX019/EX019.py
@@ -0,0 +1,4 @@
+'''DESAFIO 19:
+Um professor quer sortea um dos seus 4 alunos para apagar o quadro. Faça um programa que
+ajude ele, lendo o nome dos alunos e escrevendo o nome deles e o nome do escolhido.'''
+
