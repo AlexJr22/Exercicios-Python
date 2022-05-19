c99dfc5ce1143b3ca7e1555420dda14fc4ce047f
Alex Junior <alexjr2oficial@gmail.com>
2022-05-19 10:27:26 -0300

    Criando o EX022

---
+++ Exercicios/EX022/EX022.py
@@ -0,0 +1,6 @@
+'''DESAFIO 22:
+Faça um programa que leia o nome completo de uma pessoa e mostre:
+    - O nome com todas as letras maiúsculas
+    - O nome com todas as letras minúsculas
+    - Quantas letras ao todo sem considerar os espaços
+    - Quantas letras tem o primeiro nome'''
