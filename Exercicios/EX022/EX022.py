30a2bf757138a42c8bd417ba045624236e04d7a9
Alex Junior <alexjr2oficial@gmail.com>
2022-05-19 10:49:03 -0300

    atualizando o EX022

--- Exercicios/EX022/EX022.py
+++ Exercicios/EX022/EX022.py
@@ -4,3 +4,18 @@ Faça um programa que leia o nome completo de uma pessoa e mostre:
     - O nome com todas as letras minúsculas
     - Quantas letras ao todo sem considerar os espaços
     - Quantas letras tem o primeiro nome'''
+
+# coletando os dados e processando eles
+nome = str(input('Digite seu nome completo: ')) # recebe o nome
+primeiro_nome = nome.split()
+
+# mostrando o resultado
+print(f'Olá {primeiro_nome[0]} seu nome com todas as letras maiúsculas é: ', end='')
+print(nome.upper())
+
+print(f'Seu nome com todas as letras minúsculas é: ', end='')
+print(nome.lower())
+
+print('Seu nome tem {} letras'.format(len(nome) - nome.count(' ')))
+
+print(f'Seu primeiro nome tem {len(primeiro_nome[0])} letras')
