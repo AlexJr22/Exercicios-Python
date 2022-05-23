4977220cbf9edd8b6113c9fcb6181c444d3fe3b0
Alex Junior <alexjr2oficial@gmail.com>
2022-05-23 11:19:54 -0300

    terminando o EX025

--- Exercicios/EX025/EX025.py
+++ Exercicios/EX025/EX025.py
@@ -1,2 +1,14 @@
 '''DESAFIO 25:
 Faça um programa que leia o nome de uma pessoa e mostre no terminal se ela tem silva no nome'''
+
+# coletando os dados da pessoa e processando eles
+nome = str(input('Digite o nome de uma pessoa: ')).strip().split()
+resp = False
+for v in range(len(nome)):
+    if nome[v].lower() == 'silva':
+        print('SIM!! Essa pessoa tem "Silva" no nome!')
+        resp = True
+        break
+    
+if resp == False:
+    print('NÃO!! Essa pessoa não tem "Silva" no nome!')
