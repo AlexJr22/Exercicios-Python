c84ceeb15d77cade1f81a01ebdf681940f09a278
Alex Junior <alexjr2oficial@gmail.com>
2022-05-26 14:13:08 -0300

    Criando o EX029

---
+++ Exercicios/EX029/EX029.py
@@ -0,0 +1,4 @@
+'''DESAFIO 29:
+Escreva um programa que leia a velocidade de um carro.
+Se a velocidade ultrapassar 80KM/h, mostre uma mensagem dizendo que o motorista foi multado.
+    - A multa vai custar R$7,00 pro KM acima do limite'''
