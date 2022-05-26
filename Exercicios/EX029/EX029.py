997af7be5ad5eb0c1667d5320525fd79a7058343
Alex Junior <alexjr2oficial@gmail.com>
2022-05-26 14:23:32 -0300

    terminando o EX029

--- Exercicios/EX029/EX029.py
+++ Exercicios/EX029/EX029.py
@@ -2,3 +2,12 @@
 Escreva um programa que leia a velocidade de um carro.
 Se a velocidade ultrapassar 80KM/h, mostre uma mensagem dizendo que o motorista foi multado.
     - A multa vai custar R$7,00 pro KM acima do limite'''
+
+veloc = int(input('Qual a velocidade do veículo: '))    # recebe a velociade do veículo
+
+if veloc > 80:
+    multa = (veloc - 80) * 7                            # calcula a multa
+    print('Você foi multado!!')
+    print(f'A multa aplicada foi de R${multa}.')
+else:
+    print('Este veiculo não esta ultrapassando o limite de velocidade permitido!')
