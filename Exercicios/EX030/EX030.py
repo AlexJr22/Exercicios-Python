56b4baee63bbf4686d941bd946fcc9d4a50ae0bb
Alex Junior <alexjr2oficial@gmail.com>
2022-05-27 10:09:24 -0300

    terminando o DESAFIO

--- Exercicios/EX030/EX030.py
+++ Exercicios/EX030/EX030.py
@@ -1,3 +1,10 @@
 '''DESAFIO 30:
 Crie um programa que leia um número e mostre na tela se ele é PAR ou IMPAR'''
 
+num = int(input('Digite um número: '))  # recebe um número
+
+# validando os dados
+if num % 2 == 0:
+    print(f'O número {num} é PAR.')
+else:
+    print(f'O número {num} é IMPAR.')
