e5929b5c7a8833e9937c98464e20a59c34b546b5
Alex Junior <alexjr2oficial@gmail.com>
2022-05-27 10:06:00 -0300

    Criando o EX031

---
+++ Exercicios/EX031/EX031.py
@@ -0,0 +1,4 @@
+''' DESAFIO 31:
+Desemvolva um programa que pergunte a distância de uma viagem em km.
+Calcule o preço da passagem, cobrando R$0,50 por km para viagens de 200km, a R$0,45 para viagens mais longas'''
+
