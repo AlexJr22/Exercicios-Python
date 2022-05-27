c8296095501e19f4cbfca73ee6c922c7c39756a4
Alex Junior <alexjr2oficial@gmail.com>
2022-05-27 10:45:11 -0300

    terminando o DESAFIO

--- Exercicios/EX031/EX031.py
+++ Exercicios/EX031/EX031.py
@@ -2,3 +2,10 @@
 Desemvolva um programa que pergunte a distância de uma viagem em km.
 Calcule o preço da passagem, cobrando R$0,50 por km para viagens de 200km, a R$0,45 para viagens mais longas'''
 
+dist = float(input('Quantos KM tem a viagem?: '))   # recebe a distância da viagem
+
+# calculando o preço da viagem
+if dist >= 200:
+    print(f'Você pagara nessa viagem R${dist * 0.45}')
+else:
+    print(f'Voce pagara nessa viagem R${dist * 0.50}')
