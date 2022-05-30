9e921faa57aa8858ebff379b3faa69ce21661e8c
Alex Junior <alexjr2oficial@gmail.com>
2022-05-30 12:08:37 -0300

    criando o EX034

---
+++ Exercicios/EX034/EX034.py
@@ -0,0 +1,4 @@
+'''DESAFIO 34:
+Escreva um programa que pergunte o salario de um funcionario e calcule o valor do seu aumento.
+Para salarios acima de R$1250,00 calcule um aumento de 10% para os inferiores ou iguais o aumento é de 15%
+'''
