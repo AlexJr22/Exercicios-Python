b48343ef2de13a1c5e75734e57da21fb84bcc0ec
Alex Junior <alexjr2oficial@gmail.com>
2022-05-31 13:49:58 -0300

    atualizando o EX032

--- Exercicios/EX032/EX032.py
+++ Exercicios/EX032/EX032.py
@@ -1,8 +1,9 @@
 '''DESAFIO 32
 Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO.
 '''
-ano = int(input('Digite um ano: '))
+ano = int(input('Digite um ano: ')) # coletando o ano
 
+# processando o ano bissexto
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print(f'O ano {ano} é bissexto')
 else:
