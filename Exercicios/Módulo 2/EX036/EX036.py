9307b7500180b8585761f1e8fb3e59d32b477128
Alex Junior <alexjr2oficial@gmail.com>
2022-06-02 09:53:33 -0300

    criando o EX036

---
+++ Exercicios/Módulo 2/EX036/EX036.py
@@ -0,0 +1,5 @@
+'''DESAFIO 36:
+Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. O programa vai pergunta 
+o valor da casa e o salário do comprador e em quantos anos ele vai pagar.
+Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do valor do salário ou então o emprestimo será negado.
+'''
