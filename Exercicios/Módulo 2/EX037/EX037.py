04e12ba6a8d1caa476ae858804b8286194a35d75
Alex Junior <alexjr2oficial@gmail.com>
2022-06-02 09:56:57 -0300

    criando o EX037

---
+++ Exercicios/Módulo 2/EX037/EX037.py
@@ -0,0 +1,7 @@
+'''DESAFIO 37:
+Escreva um programa que leia um número interio qualquer e peça para o usuario escolher qual será a base 
+de conversão:
+    -> 1 para binária
+    -> 2 para octal
+    -> 3 para hexadecimal
+'''
