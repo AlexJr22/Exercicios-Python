85be374d504ddacd5736d206556c53c3566c7cdd
Alex Junior <alexjr2oficial@gmail.com>
2022-06-03 15:27:25 -0300

    criando o EX040

---
+++ Exercicios/Módulo 2/EX040/EX040.py
@@ -0,0 +1,30 @@
+'''DESAFIO 40:
+Crie um programa que leia  duas notas de um aluno e calcule sua média, mostrando um mensagem no final, de acordo
+com a média atingida:
+    -> Média abaixo de 5,0 REPROVADO
+    -> Média entre 5,0 e 6,9 RECUPERAÇÃO
+    -> Média 7,0 ou acima APROVADO
+'''
+# dados dos alunos
+nome = str(input('Qual o nome do aluno!: '))
+n1 = float(input('Primeira nota: '))
+n2 = float(input('Segunda nota: '))
+
+# resultado das provas
+
+nf = (n1 + n2) / 2 # média do aluno
+
+print('-=-' * 15)
+print('RESULTADO DAS PROVAS'.center(45))
+print('-=-' * 15)
+
+if nf < 5.0:
+    print('O aluno {} foi REPROVADO!'.format(nome))
+    print('A média do aluno foi {}'.format(nf))
+elif nf >= 5.0 and nf < 6.9:
+    print('O aluno {} está de RECUPERAÇÃO!'.format(nome))
+    print('A média do aluno foi {}'.format(nf))
+
+else:
+    print('O aluno {} foi APROVADO'.format(nome))
+    print('A média do aluno foi {}'.format(nf))
