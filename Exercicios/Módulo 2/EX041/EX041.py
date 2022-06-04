5088be932ddc309f70ad157ce3fb41bb046dc90c
Alex Junior <alexjr2oficial@gmail.com>
2022-06-04 18:59:34 -0300

    terminando o EX041

--- Exercicios/Módulo 2/EX041/EX041.py
+++ Exercicios/Módulo 2/EX041/EX041.py
@@ -10,5 +10,25 @@ cartegoria, de acordo com idade:
 
 from datetime import date
 
-nome = str(input('Nome do atleta:   '))
-ano = 
+# Dados do atleta 
+nome = str(input('Nome do atleta: '))
+ano = int(input('Em quel ano ele nasceu: '))
+
+hoje = date.today().year    # recebe o ano atual
+idade = hoje - ano          # calcula a idade do atleta
+
+# Resultado da pesquisa
+print('-=-' * 20)
+print('                 CATEGORIA DO ATLETA {}'.format(nome.upper()))
+print('-=-' * 20)
+
+if idade < 9:
+    print(f'Este atleta tem {idade} anos e sua categoria é MINI!')
+elif idade < 14:
+    print(f'Este atleta tem {idade} anos e sua categoria é INFANTIL')
+elif idade < 19:
+    print(f'Este atleta tem {idade} anos e sua categoria é JUNIOT')
+elif idade <= 20:
+    print(f'Este atleta tem {idade} anos e sua categoria é SENIOR')
+else:
+    print(f'Este atleta tem {idade} anos e sua categoria é MASTER')
