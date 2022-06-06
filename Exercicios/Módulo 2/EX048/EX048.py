c0ed3b30d448fb92dc12280ae09ed88d21c0362b
Alex Junior <alexjr2oficial@gmail.com>
2022-06-06 10:56:06 -0300

    terminando o EX048

---
+++ Exercicios/Módulo 2/EX048/EX048.py
@@ -0,0 +1,10 @@
+''' DESAFIO 48:
+Faça um programa que calcule a soma entre todos os números impares que são multiplos de três e que se encontram no 
+intervalo de 1 ate 500.
+'''
+
+c = 0
+for num in range(1,500+1,+2):
+    if num % 3 == 0:
+        c += num
+print(c)
