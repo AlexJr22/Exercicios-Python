c17d278ddd7d92aa3d35560edd067948f0e0dd4e
Alex Junior <alexjr2oficial@gmail.com>
2022-06-25 13:05:18 -0300

    terminando o Ec 66

--- Exercicios/Módulo 2/EX066/EX066.py
+++ Exercicios/Módulo 2/EX066/EX066.py
@@ -1,8 +1,19 @@
 ''' Desafio 66:
-Crie um programa que leia varios números
-pelo teclado. O programa só vai parar quando
-o usuário digitar o valor 999. que é a 
-condição de parada. no final mostre quantos 
-números foram digitados e qual foi a soma 
-entre eles.
+Crie um programa que leia varios números pelo teclado. O programa só vai parar quando o 
+usuário digitar o valor 999. Que é a condição de parada. no final mostre quantos 
+números foram digitados e qual foi a soma entre eles.
 '''
+stop = cont = sumNunbers = 0
+while stop != 999:
+  num = int(input('Digite um número: [999 para parar]'))
+  if num == 999:
+    stop = 999
+
+  else:
+    cont += 1
+    sumNunbers += num
+
+print('-=' * 15)
+print(f'Foram digitados ao todo {cont} números')
+print(f'A soma entre ele é {sumNunbers}')
+print('-=' * 15)
