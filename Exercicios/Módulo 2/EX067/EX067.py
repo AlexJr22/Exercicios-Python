7135ac4db29d20d27a9fe49d185ac97ef243d845
Alex Junior <alexjr2oficial@gmail.com>
2022-06-26 09:45:55 -0300

    criando o Ex67

---
+++ Exercicios/Módulo 2/EX067/EX067.py
@@ -0,0 +1,4 @@
+''' Desafio 67:
+Faça um programa que mostre a tabuada de varios números, um de cada vez, para cada valor digitado pelo usuario. 
+O programa será interrompido quandoo número soliciatado for negativo.
+'''
