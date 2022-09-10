940dd9b371044eefe40b6f04014a6e5ff43cf97a
Alex Junior <alexjr2oficial@gmail.com>
2022-09-10 13:12:04 -0300

    ideia inicial da tabuada automatica

--- Exercicios/Módulo 2/EX068/EX068.py
+++ Exercicios/Módulo 2/EX068/EX068.py
@@ -2,6 +2,7 @@
 Faça um programa que mostre a tabuada de varios números, um de cada vez, para cada valor digitado pelo 
 usuario. O programa será interrompido quando o número soliciatado for negativo.
 '''
+from time import sleep
 
 print('''
 -=-=-=-=-=-=-=-=-=-=
@@ -12,7 +13,12 @@ print('''
 num = int(input('''Digite um número para ver sua tabuada
 >>> '''))
 
-print('-=-=-=-=-=-=-=-=-=-=-=-=')
-for n in range(1, 10):
-  print(f'{num} X {n} = {num * n}'.center(24))
-print('-=-=-=-=-=-=-=-=-=-=-=-=')
+if num == 0:
+  print('ENCERRANDO ...')
+  sleep(1.3)
+  print('Volte Sempre!!')
+else:
+  print('-=-=-=-=-=-=-=-=-=-=-=-=')
+  for n in range(1, 10):
+    print(f'{num} X {n} = {num * n}'.center(24))
+  print('-=-=-=-=-=-=-=-=-=-=-=-=')
