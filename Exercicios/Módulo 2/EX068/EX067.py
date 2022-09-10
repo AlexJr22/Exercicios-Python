2ac2d8b1d3de8d2d1f3c888e8c7ca81f8c27b6f0
Alex Junior <alexjr2oficial@gmail.com>
2022-09-10 12:49:16 -0300

    iniciando o EX068

--- Exercicios/Módulo 2/EX068/EX067.py
+++ Exercicios/Módulo 2/EX068/EX067.py
@@ -3,3 +3,11 @@ Faça um programa que mostre a tabuada de varios números, um de cada vez, para 
 usuario. O programa será interrompido quando o número soliciatado for negativo.
 '''
 
+print('''
+-=-=-=-=-=-=-=-=-=-=
+ Gerador de Tabuada
+-=-=-=-=-=-=-=-=-=-=
+''')
+
+num = int(input('''Digite um número para ver sua tabuada
+>>>'''))
