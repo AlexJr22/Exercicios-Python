3e1607487d2569dc74991e5d8100013e9915d6d4
Alex Junior <alexjr2oficial@gmail.com>
2022-09-10 12:40:05 -0300

    criando o EX068

---
+++ Exercicios/Módulo 2/EX068/EX067.py
@@ -0,0 +1,5 @@
+'''
+Faça um programa que mostre a tabuada de varios números, um de cada vez, para cada valor digitado pelo 
+usuario. O programa será interrompido quando o número soliciatado for negativo.
+'''
+
